ef5d1bdea49cc04c390989ee478988002207288b
ygorp <ygor-nogueira@hotmail.com>
2022-05-12 21:57:31 -0300

    criando script, obtendo comando da urna, do que aparece na tela e dos botôes

---
+++ script.js
@@ -0,0 +1,22 @@
+let seuVoto = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+function clicou() {
+  
+}
+
+function branco() {
+
+}
+
+function corrige() {
+
+}
+
+function confirma() {
+
+}
