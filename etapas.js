b3312efa965e7800c7004f66269ddb12a6097aa9
ygorp <ygor-nogueira@hotmail.com>
2022-05-12 21:44:10 -0300

    criando json com as etapas da votação e com as informações dos candidatos

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: 'Ana Luiza Nogueira',
+        partido: 'SOS',
+        fotos: [{ url: 'vereador1.jpg', legenda: 'Vereador' }]
+      },
+      {
+        numero: 77222,
+        nome: 'Laura Nogueira',
+        partido: 'DoPovo',
+        fotos: [{ url: 'vereador2.jpg', legenda: 'Vereador' }]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 17,
+        nome: 'Ygor Pacheco',
+        partido: 'Patriota',
+        vice: 'Ycaro Pacheco',
+        fotos: [
+          { url: 'prefeito.jpg', legenda: 'Prefeito' },
+          { url: 'vice-prefeito.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        numero: 28,
+        nome: 'Danilo Pacheco',
+        partido: 'Dobem',
+        vice: 'Eliane Pacheco',
+        fotos: [
+          { url: 'prefeito2.jpg', legenda: 'Prefeito' },
+          { url: 'vice-prefeito2.jpg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      }
+    ]
+  }
+]
